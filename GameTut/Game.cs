16148367cbf2113e8b68f78047b0e26b73380249
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-01-29 16:28:11 +0300

    Create project

---
+++ GameTut/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GameTut
+{
+    public static class Game
+    {
+        static BufferedGraphicsContext bufGrhContext;
+        public static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+
+        }
+
+        public static void Init(Form form)
+        {
+            Graphics grh;
+            bufGrhContext = BufferedGraphicsManager.Current;
+            grh = form.CreateGraphics();
+            Width = form.Width;
+            Height = form.Height;
+            Buffer = bufGrhContext.Allocate(grh, new Rectangle(0, 0, Width, Height));
+        }
+
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            Buffer.Render();
+        }
+    }
+}
---
+++ GameTut/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GameTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form
+            {
+                Width = 800,
+                Height = 600
+            };
+
+            Game.Init(form);  
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+        }
+    }
+}
